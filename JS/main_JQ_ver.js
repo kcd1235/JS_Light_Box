ee194a2fabe28f62ff6b54b84fd1a5f2510a506e
kcd <kcd1235@naver.com>
2023-05-07 17:30:14 +0900

    변수 선언

---
+++ JS/main_JQ_ver.js
@@ -0,0 +1,12 @@
+$(function () {
+  //변수 초기화
+  var $indicator = $(".indicator button");
+  var $lightbox = $(".lightbox");
+  var $block = $("#block");
+
+  //라이트박스 표시
+
+  //이미지 전환
+
+  //라이트박스 종료
+});
