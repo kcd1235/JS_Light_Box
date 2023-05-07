76773aab12b95429aa5d716309005c5ecd88512d
kcd <kcd1235@naver.com>
2023-05-07 17:25:07 +0900

    이미지 전환

--- JS/main.js
+++ JS/main.js
@@ -7,10 +7,23 @@ var block = document.querySelector("#block"); //라이트박스 배경
 function lightbox_open(num) {
   lightbox.setAttribute("class", "active");
   block.setAttribute("class", "active");
+  change_img(num); //이미지 전환함수
+  indicator[num - 1].focus();
 }
 
 //라이트박스 닫기
 function lightbox_close() {
   lightbox.removeAttribute("class");
   block.removeAttribute("class");
+}
+
+//이미지 전환 표시 함수
+function change_img(val) {
+  var imgs = document.querySelectorAll("figure > img");
+
+  for (var i = 0; i < imgs.length; i++) {
+    imgs[i].removeAttribute("class");
+  }
+  console.log(val);
+  imgs[val - 1].setAttribute("class", "active");
 }
