59d7d0aaa30a55a892a161be9a38a442e3673617
kcd <kcd1235@naver.com>
2023-05-07 17:05:55 +0900

    라이트 박스창 열기/닫기

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+//인디케이터 초기화
+var indicator = document.querySelectorAll(".indicator button");
+var lightbox = document.querySelector("#lightbox");
+var block = document.querySelector("#block"); //라이트박스 배경
+
+//라이트박스 표시
+function lightbox_open(num) {
+  lightbox.setAttribute("class", "active");
+}
+
+//라이트박스 닫기
+function lightbox_close() {
+  lightbox.removeAttribute("class");
+}
