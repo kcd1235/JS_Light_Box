cf81a2b82d9626e8b300799ee86ca6b1f53da081
kcd <kcd1235@naver.com>
2023-05-07 17:34:48 +0900

    라이트 박스 표시 및 종료(JQuery)

--- JS/main_JQ_ver.js
+++ JS/main_JQ_ver.js
@@ -5,8 +5,18 @@ $(function () {
   var $block = $("#block");
 
   //라이트박스 표시
+  function lightbox_open(num) {
+    $lightbox.addClass("active");
+    $block.addClass("active");
+    change_img(num);
+    $indicator.eq(num).focus();
+  }
 
   //이미지 전환
 
   //라이트박스 종료
+  function lightbox_close() {
+    $lightbox.removeAttr("class");
+    $block.removeAttr("class");
+  }
 });
